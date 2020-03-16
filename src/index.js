82ecc8bea928820f0dfdf9996f4768aabf9a5e17
obscuremax <maxxam@yandex.ru>
2020-03-16 23:33:21 +0300

    resolve task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = false
+
+    while(!result) {
+        let strLength = str.length
+        for(let i=0; i<bracketsConfig.length; i++) {
+            str = str.replace(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`,"")
+        }
+        if (strLength === str.length) {
+            result = true
+        }
+    }
+    str.length === 0? result=true:result=false
+
+    return result
 }
